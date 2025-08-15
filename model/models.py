f34783c185f40219a0b8fe72ab2f8d670d20faf8
Ankita-Codee <admane.ankita@gmail.com>
2025-08-15 12:55:00 -0700

    document comparison file has been updated

--- model/models.py
+++ model/models.py
@@ -11,6 +11,15 @@ class Metadata(BaseModel):
     Publisher: str
     Language: str
     PageCount: Union[int, str]  # Can be "Not Available"
-    SentimentTone: str  
+    SentimentTone: str
+class ChangeFormat(BaseModel):
+    Page: str
+    Changes: str
 
-        
+class SummaryResponse(RootModel[list[ChangeFormat]]):
+    pass
+class PromptType(str, Enum):
+    DOCUMENT_ANALYSIS = "document_analysis"
+    DOCUMENT_COMPARISON = "document_comparison"
+    CONTEXTUALIZE_QUESTION = "contextualize_question"
+    CONTEXT_QA = "context_qa"
